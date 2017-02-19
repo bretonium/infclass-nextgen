necropotame/teeworlds-infclass

commit 6649e4784883c069099580165a2ee50639b07c93

Change Target when Target afk (#163)
* Change Target when Target afk

* added config vars

* name changes and fixes

* check for valid pointers

src/game/server/gamecontext.cpp
@@ -835,6 +835,14 @@ void CGameContext::OnTick()
 		m_TargetToKill = -1;
 	}
 	
+	int LastTarget = -1;
+	// Zombie is in InfecZone too long -> change target
+	if(m_apPlayers[m_TargetToKill] && m_apPlayers[m_TargetToKill]->GetCharacter() && m_TargetToKill >= 0 && (m_apPlayers[m_TargetToKill]->GetCharacter()->GetInfZoneTick()*Server()->TickSpeed()) > 1000*g_Config.m_InfNinjaTargetAfkTime) 
+	{
+		LastTarget = m_TargetToKill;
+		m_TargetToKill = -1;
+	}
+	
 	if(m_TargetToKillCoolDown > 0)
 		m_TargetToKillCoolDown--;
 	
@@ -842,17 +850,33 @@ void CGameContext::OnTick()
 	{
 		int m_aTargetList[MAX_CLIENTS];
 		int NbTargets = 0;
+		int infectedCount = 0;
 		for(int i=0; i<MAX_CLIENTS; i++)
 		{		
 			if(m_apPlayers[i] && m_apPlayers[i]->IsInfected() && m_apPlayers[i]->GetClass() != PLAYERCLASS_UNDEAD)
 			{
-				m_aTargetList[NbTargets] = i;
-				NbTargets++;
+				if (m_apPlayers[i]->GetCharacter() && (m_apPlayers[i]->GetCharacter()->GetInfZoneTick()*Server()->TickSpeed()) < 1000*g_Config.m_InfNinjaTargetAfkTime) // Make sure zombie is not camping in InfZone
+				{
+					m_aTargetList[NbTargets] = i;
+					NbTargets++;
+				} 
+				infectedCount++;
 			}
 		}
 		
-		if(NbTargets > 1)
+		if(NbTargets > 0)
 			m_TargetToKill = m_aTargetList[random_int(0, NbTargets-1)];
+			
+		if(m_TargetToKill == -1)
+		{
+			if (LastTarget >= 0)
+				m_TargetToKill = LastTarget; // Reset Target if no new targets were found
+		}
+		
+		if (infectedCount < g_Config.m_InfNinjaMinInfected)
+		{
+			m_TargetToKill = -1; // disable target system
+		}
 	}
 	
 	//Check for banvote

src/engine/shared/config_variables.h
@@ -133,6 +133,8 @@ MACRO_CONFIG_INT(InfBarrierTimeReduce, inf_barrier_timereduce, 150, 0, 10000, CF
 MACRO_CONFIG_INT(InfMineRadius, inf_mine_radius, 46, 0, 1024, CFGFLAG_SERVER, "Radius of mines")
 MACRO_CONFIG_INT(InfMineLimit, inf_mine_limit, 2, 0, 1024, CFGFLAG_SERVER, "Maximum number of mines per player")
 MACRO_CONFIG_INT(InfNinjaJump, inf_ninja_jump, 2, 0, 1024, CFGFLAG_SERVER, "Maximum number of katana attacks")
+MACRO_CONFIG_INT(InfNinjaMinInfected, inf_ninja_min_infected, 2, 1, 15, CFGFLAG_SERVER, "Minimum number of infected to activate Target system")
+MACRO_CONFIG_INT(InfNinjaTargetAfkTime, inf_ninja_target_afk_time, 15, 1, 10000, CFGFLAG_SERVER, "How long before an afk zombie in the infection zone doesnt count as target anymore")
 MACRO_CONFIG_INT(InfPoisonDamage, inf_poison_damage, 8, 0, 1024, CFGFLAG_SERVER, "Damage deals by the poison grenades")
 MACRO_CONFIG_INT(InfGhoulDigestion, inf_ghoul_digestion, 5, 0, 10000, CFGFLAG_SERVER, "Time for a ghoul to digest an infected (sec)")
 MACRO_CONFIG_INT(InfGhoulStomachSize, inf_ghoul_stomach_size, 120, 0, 10000, CFGFLAG_SERVER, "Number of dead that the ghoul can eat")

src/game/server/entities/character.cpp
@@ -84,6 +84,7 @@ CCharacter::CCharacter(CGameWorld *pWorld)
 	m_PositionLockAvailable = false;
 	m_PoisonTick = 0;
 	m_HealTick = 0;
+	m_InfZoneTick = -1;
 	m_InAirTick = 0;
 	m_InWater = 0;
 	m_BonusTick = 0;
@@ -1439,13 +1440,18 @@ void CCharacter::Tick()
 				if(Server()->Tick() >= m_HealTick + (Server()->TickSpeed()/g_Config.m_InfInfzoneHealRate))
 				{
 					m_HealTick = Server()->Tick();
-					if(m_Health < 10) m_Health++;
+					IncreaseHealth(1);
 				}
+				if (m_InfZoneTick < 0) m_InfZoneTick = Server()->Tick(); // Save Tick when zombie enters infection zone
 			}
 			else
 			{
 				m_pPlayer->StartInfection();
 			}
+		}
+		if(m_Alive && (Index0 != ZONE_DAMAGE_INFECTION && Index1 != ZONE_DAMAGE_INFECTION && Index2 != ZONE_DAMAGE_INFECTION && Index3 != ZONE_DAMAGE_INFECTION))
+		{
+			m_InfZoneTick = -1;// Reset Tick when zombie is not in infection zone
 		}
 	}
 	
@@ -3274,5 +3280,11 @@ int CCharacter::GetInfWeaponID(int WID)
 		return INFWEAPON_NINJA;
 	}
 	else return INFWEAPON_NONE;
+}
+
+int CCharacter::GetInfZoneTick() // returns how many ticks long a player is already in InfZone
+{
+	if (m_InfZoneTick < 0) return 0;
+	return Server()->Tick()-m_InfZoneTick;
 }
 /* INFECTION MODIFICATION END *****************************************/

src/game/server/entities/character.h
@@ -171,6 +171,7 @@ private:
 	bool m_IsInvisible;
 	int m_HealTick;
 	int m_BonusTick;
+	int m_InfZoneTick;
 	
 	int m_FlagID;
 	int m_HeartID;
@@ -228,6 +229,7 @@ public:
 	bool FindWitchSpawnPosition(vec2& Res);
 	void SaturateVelocity(vec2 Force, float MaxSpeed);
 	void GiveGift(int GiftType);
+	int GetInfZoneTick();
 /* INFECTION MODIFICATION END *****************************************/
 };
 

bretonium/infclass-nextgen

commit 5912d26b85754c09a07b39462d59099550c27d6e

Fix crashed introduced in 6649e4784883c069099580165a2ee50639b07c93.

src/game/server/gamecontext.cpp
@@ -838,7 +838,7 @@ void CGameContext::OnTick()
 	
 	int LastTarget = -1;
 	// Zombie is in InfecZone too long -> change target
-	if(m_apPlayers[m_TargetToKill] && m_apPlayers[m_TargetToKill]->GetCharacter() && m_TargetToKill >= 0 && (m_apPlayers[m_TargetToKill]->GetCharacter()->GetInfZoneTick()*Server()->TickSpeed()) > 1000*g_Config.m_InfNinjaTargetAfkTime) 
+	if(m_TargetToKill >= 0 && m_apPlayers[m_TargetToKill] && m_apPlayers[m_TargetToKill]->GetCharacter() && m_TargetToKill >= 0 && (m_apPlayers[m_TargetToKill]->GetCharacter()->GetInfZoneTick()*Server()->TickSpeed()) > 1000*g_Config.m_InfNinjaTargetAfkTime) 
 	{
 		LastTarget = m_TargetToKill;
 		m_TargetToKill = -1;