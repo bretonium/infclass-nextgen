156ec41569d7c3e8f6776ac900da22f306743f72
Manmellon <24492231+Manmellon@users.noreply.github.com>
2019-01-11 21:06:31 +0300

    Added stackprotector checking for windows

--- bam.lua
+++ bam.lua
@@ -179,6 +179,10 @@ function build(settings)
 		if family == "windows" then
 			-- disable visibility attribute support for gcc on windows
 			settings.cc.defines:Add("NO_VIZ")
+			if config.stackprotector.value then
+				settings.cc.flags:Add("-fstack-protector", "-fstack-protector-all")
+				settings.link.flags:Add("-fstack-protector", "-fstack-protector-all")
+			end
 		elseif platform == "macosx" then
 			settings.cc.flags:Add("-mmacosx-version-min=10.5")
 			settings.link.flags:Add("-mmacosx-version-min=10.5")
@@ -201,7 +205,8 @@ function build(settings)
 			settings.link.frameworks:Add("AppKit")
 		else
 			settings.link.libs:Add("pthread")
-			settings.link.libs:Add("maxminddb")  -- for ip geolocation
+			--settings.link.libs:Add("geolite2++") -- for ip geolocation
+			--settings.link.libs:Add("maxminddb")  -- for ip geolocation
 			-- add ICU for linux
 			if ExecuteSilent("pkg-config icu-uc icu-i18n") == 0 then
 			end
@@ -316,8 +321,7 @@ function build(settings)
 	game_client = Compile(settings, CollectRecursive("src/game/client/*.cpp"), client_content_source)
 	game_server = Compile(settings, CollectRecursive("src/game/server/*.cpp"), server_content_source)
 	game_editor = Compile(settings, Collect("src/game/editor/*.cpp"))
-	infclassr = Compile(settings, Collect("src/infclassr/*.cpp", "src/infclassr/GeoLite2PP/*.cpp"))
-
+	infclassr = Compile(settings, Collect("src/infclassr/*.cpp"))
 
 	-- build tools (TODO: fix this so we don't get double _d_d stuff)
 	tools_src = Collect("src/tools/*.cpp", "src/tools/*.c")
