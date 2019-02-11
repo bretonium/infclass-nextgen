07ac8f8dee5db73a2f37990485c3c18498246558
duralakun <duralakun@gmail.com>
2019-02-11 20:54:09 +0100

    added null pointer check to hero flag indicator

--- src/game/server/entities/character.cpp
+++ src/game/server/entities/character.cpp
@@ -3161,7 +3161,7 @@ void CCharacter::Snap(int SnappingClient)
 				pObj->m_Type = WEAPON_HAMMER;
 			}
 		}
-		else if(GetClass() == PLAYERCLASS_HERO && g_Config.m_InfHeroFlagIndicator) 
+		else if(GetClass() == PLAYERCLASS_HERO && g_Config.m_InfHeroFlagIndicator && m_pHeroFlag) 
 		{
 			CHeroFlag *pFlag = m_pHeroFlag;
 			
