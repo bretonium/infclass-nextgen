51f84597697b00338adb1293224fc5e7ae0b29a5
Boris Bobrov <breton@cynicmansion.ru>
2019-06-21 14:24:52 +0200

    Revert "Do not send requestinfo response if ratelimited"
    This reverts commit e465291ae77367cb744d4add80db2df3f3ad1822.

--- src/engine/server/server.cpp
+++ src/engine/server/server.cpp
@@ -1529,22 +1529,14 @@ void CServer::SendServerInfoConnless(const NETADDR *pAddr, int Token, bool Exten
 		m_ServerInfoNumRequests = 1;
 		m_ServerInfoFirstRequest = Now;
 	}
+	bool SendClients = m_ServerInfoNumRequests <= MaxRequests && !m_ServerInfoHighLoad;
+	SendServerInfo(pAddr, Token, Extended, SendClients);
 
-	bool SendResponse = m_ServerInfoNumRequests <= MaxRequests && !m_ServerInfoHighLoad;
-	if(!SendResponse) {
-		char aBuf[256]
-		char aAddrStr[256];
-		net_addr_str(pAddr, aAddrStr, sizeof(aAddrStr), true);
-		str_format(aBuf, sizeof(aBuf), "Too much info requests from %s: %d > %d (Now = %d, mSIFR = %d)",
-				aAddrStr, m_ServerInfoNumRequests, MaxRequests, Now, m_ServerInfoFirstRequest);
-		Console()->Print(IConsole::OUTPUT_LEVEL_DEBUG, "inforequests", aBuf);
-	} else {
+	if(!SendClients) {
 		char aBuf[256];
-		char aAddrStr[22];
-		net_addr_str(pAddr, aAddrStr, sizeof(aAddrStr), true);
-		str_format(aBuf, sizeof(aBuf), "Accepting info requests from %s", aAddrStr);
+		str_format(aBuf, sizeof(aBuf), "Too much info requests: %d > %d (Now = %d, mSIFR = %d)", m_ServerInfoNumRequests, MaxRequests, Now,
+				   m_ServerInfoFirstRequest);
 		Console()->Print(IConsole::OUTPUT_LEVEL_DEBUG, "inforequests", aBuf);
-		SendServerInfo(pAddr, Token, Extended, true);
 	}
 }
 
